4505db6b17b13fd4a3a8e77cfc24d2684bd41e61
ARYOSVALLDO CLEEF de SOUZA <x10487855485@tcu.gov.br>
2019-04-02 15:05:19 -0300

    listando eventos

--- src/main/java/com/yout/evento/app/controller/EventoController.java
+++ src/main/java/com/yout/evento/app/controller/EventoController.java
@@ -4,24 +4,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.yout.evento.app.model.Evento;
 import com.yout.evento.app.repository.EventoRepository;
 
 @Controller // indica ao spring que essa classe é um controller
 public class EventoController {
-	
+
 	@Autowired // realiza injeção de dependencias
 	private EventoRepository er;
-	
-	@RequestMapping(value="/addEvento", method=RequestMethod.GET) // busca a view de acordo com a requisição
-	public String getFormAdd(){
+
+	@RequestMapping(value = "/addEvento", method = RequestMethod.GET) // busca a view de acordo com a requisição
+	public String getFormAdd() {
 		return "evento/formAdd.html";
 	}
-	
-	@RequestMapping(value="/addEvento", method=RequestMethod.POST)
-	public String getFormAdd(Evento evento){
+
+	@RequestMapping(value = "/addEvento", method = RequestMethod.POST)
+	public String getFormAdd(Evento evento) {
 		er.save(evento);
-		return "redirect:/addEvento";
+		return "redirect:/eventos";
+	}
+
+	@RequestMapping(value = "/eventos")
+	public ModelAndView getListEventos() { // listando eventos na view index
+		ModelAndView mv = new ModelAndView("index.html");
+		Iterable<Evento> eventos = er.findAll();
+		mv.addObject("eventos", eventos);
+		return mv;
 	}
 }
